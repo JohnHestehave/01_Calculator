7d4091f50f935ceb10783939dd8b0fd7705d9f45
John <John@JOHN>
2016-09-13 11:21:33 +0200

    Add project files.

---
+++ 01_Calculator/Calculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _01_Calculator
+{
+    public class Calculator
+    {
+        public static int Sum(int[] numbers)
+        {
+           //return numbers[0] + numbers[1];
+			int i = 0;
+			foreach (int h in numbers)
+			{
+				i += h;
+			}
+			return i;
+        }
+
+        public static int Add(int a1, int a2)
+        {
+            return a1 + a2;
+        }
+
+        public static int Subtract(int s1, int s2)
+        {
+            return s1 - s2;
+        }
+
+        public static int Multiply(int m1, int m2)
+        {
+            return m1 * m2;
+        }
+
+        public static double Power(int p1, int p2)
+        {
+            return Math.Pow(p1, p2);
+        }
+
+        public static int Factorial(int x1)
+        {
+            int sum = x1;
+             for (int x = x1-1; x > 0; x--)
+            {
+                sum = sum * x;
+            }
+            return sum;
+        }
+    }
+}
---
+++ CalculatorApplication/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using _01_Calculator;
+
+namespace CalculatorApplication
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] sum1 = new int[3] { 1, 2, 3 };
+			int res = Calculator.Sum(sum1);
+			Console.WriteLine(res);
+			Regex o = new Regex(@"[^\d]");
+			Console.WriteLine("Indtast et tal:");
+			string input = Console.ReadLine();
+			input = o.Replace(input, "");
+			int i = Convert.ToInt32(input);
+			Console.WriteLine("Næste tal:");
+			string input2 = Console.ReadLine();
+			input2 = o.Replace(input2, "");
+			int i2 = Convert.ToInt32(input2);
+			Console.WriteLine(i+i2);
+			Console.ReadLine();
+			
+		}
+	}
+}
